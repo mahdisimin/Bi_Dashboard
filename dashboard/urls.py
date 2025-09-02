96e43c9b34ab3457c321d3fc708f3887b21ef407
Mahdi Simin <MohammadMahdisimin@gmail.com>
2025-09-03 00:15:22 +0330

    step3 is done

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,9 +1,15 @@
+from dashboard.views import book_list
 from django.urls import path
-from .views import home , about
+from .views import home , about ,query_list , query_details
 
 app_name = 'dashboard'
 
 urlpatterns = [
     path('', home, name='home'),
     path('about/', about , name='about'),
+
+    path('queries/', query_list , name='query_list'),
+    path('queries/<slug:slug>/', query_details , name='query_details'),
+
+    path('books/', book_list , name='book_list'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
 from django.http import  HttpResponse
 
@@ -11,3 +11,46 @@ def home(request):
 def about(request):
     return render(request, 'dashboard/about.html')
 
+def query_list(request):
+    ctx = {"queries" : Queries}
+    return render(request, 'dashboard/query_list.html', ctx)
+
+def query_details(request , slug):
+    item = next((x for x in Queries if x['slug'] == slug), None)
+    if not  item :
+        raise Http404("آیتم یافت نشد")
+    return render(request, 'dashboard/query_detail.html', {"q": item})
+
+def book_list(request):
+    ctx = {'book_list':books}
+    return render(request, 'dashboard/book_list.html', ctx)
+
+Queries = [
+    {
+        "slug": "monthly-sales",
+        "title": "فروش ماهانه",
+        "description": "جمع فروش به تفکیک ماه",
+        "sample_sql": "SELECT month, SUM(amount) FROM sales GROUP BY month;"
+    } ,
+    {
+        "slug": "top-5-products",
+        "title": "۵ محصول برتر",
+        "description": "بیشترین فروش بر اساس مبلغ",
+        "sample_sql": "SELECT product, SUM(amount) as total FROM sales GROUP BY product ORDER BY total DESC LIMIT 5;",
+    }
+]
+
+books = [
+    {
+        "slug": "SL1",
+        "title": "T1",
+        "author": "A1",
+        "summary": "S1",
+    },
+    {
+        "slug": "SL2",
+        "title": "T2",
+        "author": "A2",
+        "summary": "S2",
+    }
+]
